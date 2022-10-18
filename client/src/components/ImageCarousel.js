110e155111576e7db935a13dd57b4e71cfab9ce0
Arnesh Bhattacharya <arnesh1701@gmail.com>
2022-10-19 00:36:51 +0530

    Added product catalogue

---
+++ client/src/components/ImageCarousel.js
@@ -0,0 +1,84 @@
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+
+import React from "react";
+import ProductCard from "./ProductCard";
+import "./Products.css";
+
+const ImageCarousel = ({Heading}) => {
+  return (
+    <div>
+      <div className="Heading">
+        {Heading}
+      </div>
+      <Carousel
+        additionalTransfrom={0}
+        arrows
+        autoPlaySpeed={3000}
+        centerMode={false}
+        className=""
+        containerClass="container-with-dots"
+        dotListClass=""
+        draggable
+        focusOnSelect={false}
+        infinite
+        itemClass="carousel-item-padding-4-px"
+        keyBoardControl
+        minimumTouchDrag={80}
+        pauseOnHover
+        renderArrowsWhenDisabled={false}
+        renderButtonGroupOutside={false}
+        renderDotsOutside={false}
+        responsive={{
+          superLargeDesktop: {
+            // the naming can be any, depends on you.
+            breakpoint: { max: 4000, min: 3000 },
+            items: 6,
+          },
+          desktop: {
+            breakpoint: {
+              max: 3000,
+              min: 1024,
+            },
+            items: 4,
+            partialVisibilityGutter: 40,
+          },
+          mobile: {
+            breakpoint: {
+              max: 464,
+              min: 0,
+            },
+            items: 1,
+            partialVisibilityGutter: 30,
+          },
+          tablet: {
+            breakpoint: {
+              max: 1024,
+              min: 464,
+            },
+            items: 2,
+            partialVisibilityGutter: 30,
+          },
+        }}
+        rewind={false}
+        rewindWithAnimation={false}
+        rtl={false}
+        shouldResetAutoplay
+        showDots={false}
+        sliderClass=""
+        slidesToSlide={1}
+        swipeable
+      >
+      <ProductCard name="AB" price="100$" user="sd"/>
+      <ProductCard name="AB" price="100$" user="sd"/>
+      <ProductCard name="AB" price="100$" user="sd"/>
+      <ProductCard name="AB" price="100$" user="sd"/>
+      <ProductCard name="AB" price="100$" user="sd"/>
+      <ProductCard name="AB" price="100$" user="sd"/>
+      <ProductCard name="AB" price="100$" user="sd"/>
+      </Carousel>
+    </div>
+  );
+};
+
+export default ImageCarousel;
