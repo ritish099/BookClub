4a16f98dbdaed57e468bfa6ababe050bb3b52172
Souraprabha Ganguly <souraprabhaganguly26@gmail.com>
2022-10-23 00:20:53 +0530

    added server configuration

--- server/src/index.js
+++ server/src/index.js
@@ -1,5 +1,16 @@
-const appName = "Server API"; 
-const port = process.env.PORT || 8080;
-const createServer = require("./server");
-const server = createServer();
-server.listen(port, () => console.log(`${appName} running on port ${port}!`));
+// The entry point to our server
+const PORT = '5000';
+
+const express = require('express');
+const app = express();
+const productRouter = require('./routes/products.js');
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('/api/products', productRouter);
+//
+
+app.listen(PORT, () => {
+    console.log(`Server running sucessfully on PORT ${PORT}`);
+})
