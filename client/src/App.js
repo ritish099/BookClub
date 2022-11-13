253ee536a60e87f387f15cd7db8ac279ae39a94f
Ritish Singh <ritish.singh@crib.in>
2022-11-13 12:54:14 +0530

    done

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,6 @@ import {ChakraProvider} from "@chakra-ui/react";
 
 import router from "./router/routes";
 import {RouterProvider} from "react-router-dom";
-import BookUpload from "./components/BookUpload.js"
 
 function App() {
   return (
