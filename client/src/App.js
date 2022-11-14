b5415241544e5072c1946cc24298c19b20a93321
Souraprabha Ganguly <souraprabhaganguly26@gmail.com>
2022-11-14 07:24:55 +0530

    added form validation and connected signup

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,18 @@ import {ChakraProvider} from "@chakra-ui/react";
 
 import router from "./router/routes";
 import {RouterProvider} from "react-router-dom";
+import userContext from "./context/userContext";
+import {useState} from "react";
 
 function App() {
+  const [user, setUser] = useState({token: null});
   return (
     <>
-      <ChakraProvider>
-        <RouterProvider router={router}></RouterProvider>
-      </ChakraProvider>
+      <userContext.Provider value={{user, setUser}}>
+        <ChakraProvider>
+          <RouterProvider router={router}></RouterProvider>
+        </ChakraProvider>
+      </userContext.Provider>
     </>
   );
 }
--- client/src/components/Signin.js
+++ client/src/components/Signin.js
@@ -4,16 +4,19 @@ import {
   FormControl,
   FormLabel,
   Input,
-  Checkbox,
   Stack,
   Button,
   Heading,
   Text,
   useColorModeValue,
+  FormErrorMessage,
+  FormHelperText,
 } from "@chakra-ui/react";
-import { Link } from 'react-router-dom';
+import {Link} from "react-router-dom";
+import {Field, Form, Formik} from "formik";
+import LoginSchema from "../schema/LoginSchema";
 
-export default function SimpleCard() {
+export default function SignupCard() {
   return (
     <Flex
       minH={"100vh"}
@@ -23,51 +26,95 @@ export default function SimpleCard() {
     >
       <Stack spacing={8} mx={"auto"} maxW={"lg"} py={12} px={6}>
         <Stack align={"center"}>
-          <Heading fontSize={"4xl"}>Sign in to your account</Heading>
+          <Heading fontSize={"4xl"} textAlign={"center"}>
+            Sign in
+          </Heading>
           <Text fontSize={"lg"} color={"gray.600"}>
-            to enjoy all of our cool <Link color={"blue.400"}>features</Link> ✌️
+            to enjoy all of our cool features ✌️
           </Text>
         </Stack>
+
         <Box
+          alignSelf={"center"}
           rounded={"lg"}
           bg={useColorModeValue("white", "gray.700")}
           boxShadow={"lg"}
-          p={8}
+          p={50}
         >
-          <Stack spacing={4}>
-            <FormControl id="email">
-              <FormLabel>Email address</FormLabel>
-              <Input type="email" />
-            </FormControl>
-            <FormControl id="password">
-              <FormLabel>Password</FormLabel>
-              <Input type="password" />
-            </FormControl>
-            <Stack spacing={10}>
-              <Stack
-                direction={{base: "column", sm: "row"}}
-                align={"start"}
-                justify={"space-between"}
-              >
-                <Checkbox>Remember me</Checkbox>
-                <Link color={"blue.400"}>Forgot password?</Link>
-              </Stack>
-              <Button
-                bg={"blue.400"}
-                color={"white"}
-                _hover={{
-                  bg: "blue.500",
-                }}
-              >
-                Sign in
-              </Button>
-              <Stack pt={6}>
-                <Text align={"center"}>
-                  Don't have an account? <Link to="/signup">Signup</Link>
-                </Text>
-              </Stack>
-            </Stack>
-          </Stack>
+          <Formik
+            validationSchema={LoginSchema}
+            initialValues={{
+              email: "",
+              password: "",
+            }}
+            onSubmit={(values, actions) => {
+              setTimeout(() => {
+                alert(JSON.stringify(values, null, 2));
+                actions.setSubmitting(false);
+              }, 1000);
+            }}
+          >
+            {(props) => (
+              <Form autoComplete="false">
+                <Stack py={3} px={6}>
+                  <Field name="email">
+                    {({field, form}) => (
+                      <FormControl isInvalid={form.errors.email} isRequired>
+                        <FormLabel>Email</FormLabel>
+                        <Input {...field} placeholder="Enter Email" />
+                        {form.errors.email ? (
+                          <FormErrorMessage w={150}>
+                            {form.errors.email}
+                          </FormErrorMessage>
+                        ) : (
+                          <FormHelperText w={150}></FormHelperText>
+                        )}
+                      </FormControl>
+                    )}
+                  </Field>
+                </Stack>
+
+                <Stack py={3} px={6}>
+                  <Field name="password">
+                    {({field, form}) => (
+                      <FormControl isInvalid={form.errors.password} isRequired>
+                        <FormLabel>Password</FormLabel>
+                        <Input
+                          type="password"
+                          {...field}
+                          placeholder="Enter Password"
+                        />
+                        {form.errors.password ? (
+                          <FormErrorMessage w={150}>
+                            {form.errors.password}
+                          </FormErrorMessage>
+                        ) : (
+                          <FormHelperText w={150}></FormHelperText>
+                        )}
+                      </FormControl>
+                    )}
+                  </Field>
+                </Stack>
+
+                <Stack align={"center"}>
+                  <Button
+                    mt={4}
+                    colorScheme="teal"
+                    isLoading={props.isSubmitting}
+                    type="submit"
+                  >
+                    Sign in
+                  </Button>
+                </Stack>
+
+                <Stack pt={6}>
+                  <Text align={"center"}>
+                    Don't have an account? <Link to="/signup">Sign up</Link>
+                  </Text>
+                </Stack>
+              </Form>
+            )}
+          </Formik>
         </Box>
       </Stack>
     </Flex>
