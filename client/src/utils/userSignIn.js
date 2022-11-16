7527fe3250a6ab5da6441cabcb3ee9950b275d05
Souraprabha Ganguly <souraprabhaganguly26@gmail.com>
2022-11-16 18:49:44 +0530

    added signout functionality

--- client/src/utils/userSignIn.js
+++ client/src/utils/userSignIn.js
@@ -7,19 +7,24 @@ const userSignIn = (data, actions, setSignInSuccess, setSignInError, navigate) =
   axios
     .post(url, data)
     .then((res) => {
-      console.log("suc", res.data.data.token);
+      console.log("suc", res.data.data.profile);
       console.log('Successfully signed in')
       actions.setSubmitting(false);
       setSignInSuccess({success: true, message: ""});
       setSignInError({error: false, message: ""});
-      saveToLocalStorage(res.data.data.token);
+      saveToLocalStorage(res.data.data.profile.name, res.data.data.token);
       navigate("/profile")
     })
     .catch((err) => {
       console.log("err", err);
       actions.setSubmitting(false);
       setSignInSuccess({success: false, message: ""});
-      setSignInError({error: true, message: err.response.data.message});
+
+      if (err.code === "ERR_NETWORK") {
+        setSignInError({error: true, message: "network error"});
+      } else {
+        setSignInError({error: true, message: err.response.data.message});
+      }
     });
 };
 
