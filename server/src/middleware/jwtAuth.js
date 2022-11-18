d765cced6095ec51078d69848552463b4b0dbb41
Arnesh Bhattacharya <arnesh1701@gmail.com>
2022-11-19 02:43:33 +0530

    Added user profile

--- server/src/middleware/jwtAuth.js
+++ server/src/middleware/jwtAuth.js
@@ -4,6 +4,7 @@ import config from "../../config/config.js";
 const jwtAuth = (req, res, next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
+        console.log('109090909000090000000',token);
         if (token) {
             const decodedData = jwt.verify(token, config.JWT_ACTIVATE);
             req.userId = decodedData?.id;
