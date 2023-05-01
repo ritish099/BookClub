3548c2b05b59649d7011466a5e5127df70e23c30
Souraprabha Ganguly <souraprabhaganguly26@gmail.com>
2023-05-02 03:23:40 +0530

    implemented Notes Download Page

--- client/src/components/NotesCard.js
+++ client/src/components/NotesCard.js
@@ -2,72 +2,42 @@ import {
   Box,
   Center,
   Text,
-  Stack,
   Heading,
   List,
-  ListItem,
-  ListIcon,
   Button,
   useColorModeValue,
 } from "@chakra-ui/react";
-import {CheckIcon} from "@chakra-ui/icons";
 
-export default function NotesCard({title, userName }) {
+export default function NotesCard({note}) {
   return (
     <Center py={6}>
       <Box
-        maxW={"330px"}
+        minW={"300px"}
         w={"full"}
         bg={useColorModeValue("white", "gray.800")}
         boxShadow={"2xl"}
         rounded={"md"}
         overflow={"hidden"}
       >
-        {/* <Stack
-          textAlign={"center"}
-          p={6}
-          color={useColorModeValue("gray.800", "white")}
-          align={"center"}
-        >
-          <Text
-            fontSize={"sm"}
-            fontWeight={500}
-            bg={useColorModeValue("green.50", "green.900")}
-            p={2}
-            px={3}
-            color={"green.500"}
-            rounded={"full"}
-          >
-            Hobby
-          </Text>
-          <Stack direction={"row"} align={"center"} justify={"center"}>
-            <Text fontSize={"3xl"}>$</Text>
-            <Text fontSize={"6xl"} fontWeight={800}>
-              79
-            </Text>
-            <Text color={"gray.500"}>/month</Text>
-          </Stack>
-        </Stack> */}
-
         <Box bg={useColorModeValue("gray.50", "gray.900")} px={6} py={10}>
           <List spacing={3}>
             <Heading fontSize={"2xl"} fontFamily={"body"}>
-              {"title"}
+              {note.notesTitle}
             </Heading>
             <Text fontWeight={600} color={"gray.500"} mb={4}>
-              {"Arnesh"}
+              {note.ownerName}
             </Text>
             <Text fontWeight={600} mb={4}>
               <Text fontWeight={600} color={"gray.500"}>
                 {"Branch: "}
               </Text>
-              {"Computer Science and Engineering"}
+              {note.branch}
             </Text>
             <Text fontWeight={600} mb={4}>
               <Text fontWeight={600} color={"gray.500"}>
                 {"Subject: "}
               </Text>
-              {"Data Structures"}
+              {note.subject}
             </Text>
           </List>
 
@@ -84,6 +54,7 @@ export default function NotesCard({title, userName }) {
             _focus={{
               bg: "green.500",
             }}
+            onClick={() => {}}
           >
             Download
           </Button>
--- client/src/components/NotesDownload.js
+++ client/src/components/NotesDownload.js
@@ -2,21 +2,26 @@ import React, {useState, useEffect} from "react";
 import "../styles/Products.css";
 import NotesCard from './NotesCard';
 import {Select, Text} from "@chakra-ui/react";
+import getAllNotes from "../utils/getAllNotes";
 
 const NotesDownload = () => {
-  const [Notes, setNotes] = useState([]);
+  const [allNotes, setAllNotes] = useState([]);
   const [semester, setSemester] = useState('');
+  const [displayNotes, setDisplayNotes] = useState([]);
 
-//   useEffect(() => {
-//     async function getData() {
-//       await getAllBooks(setBooks);
-//     }
-//     getData();
-//   }, []);
+  useEffect(() => {
+    async function getData() {
+      await getAllNotes(setAllNotes, setDisplayNotes);
+    }
+    getData();
+  }, []);
+
+  useEffect(() => {
+    setDisplayNotes(allNotes.filter(ele => ele.semester === semester))
+  }, [semester])
 
   return (
     <div className="BigDiv">
-
       <div className="Heading">
         <Text marginBottom={"10px"}>Download notes</Text>
       </div>
@@ -28,17 +33,26 @@ const NotesDownload = () => {
         width={"300px"}
         mt={10}
         mb={10}
+        onChange={(e) => {
+          setSemester(e.target.value);
+        }}
       >
-        <option value="option1">Option 1</option>
-        <option value="option2">Option 2</option>
-        <option value="option3">Option 3</option>
+        <option value="semester1">Semester 1</option>
+        <option value="semester2">Semester 2</option>
+        <option value="semester3">Semester 3</option>
+        <option value="semester4">Semester 4</option>
+        <option value="semester5">Semester 5</option>
+        <option value="semester6">Semester 6</option>
+        <option value="semester7">Semester 7</option>
+        <option value="semester8">Semester 8</option>
       </Select>
 
       <div className="Catalogue">
-        <NotesCard />
-        <NotesCard />
-        <NotesCard />
-        <NotesCard />
+        {displayNotes.length ? (
+          displayNotes.map((note) => <NotesCard note={note} />)
+        ) : (
+          <Text marginBottom={"10px"}>No notes found for this semester</Text>
+        )}
       </div>
     </div>
   );
