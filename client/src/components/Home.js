3df820e88a1c9f0a3ce9328215db42a7130d851f
Souraprabha Ganguly <souraprabhaganguly26@gmail.com>
2023-05-14 06:20:03 +0530

    added payment failure handler

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -5,10 +5,12 @@ import SidebarWithHeader from "./Sidebar";
 import {useSearchParams} from "react-router-dom";
 import {useNavigate} from "react-router-dom";
 import {useEffect} from "react";
+import { useToast } from "@chakra-ui/react";
 
 const Home = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
+  const toast = useToast();
 
   useEffect(() => {
     async function verifyUserEmail() {
@@ -25,7 +27,13 @@ const Home = () => {
       if (paymentMessage === "payment-success") {
         navigate("/success");
       } else if (paymentMessage === "payment-failure") {
-        //do later
+        toast({
+          title: "Payment failed! Please try again.",
+          status: "error",
+          position: "bottom-left",
+          duration: 9000,
+          isClosable: true,
+        });
       }
     }
 
